f41cf6ec2fd63830abea79941097a6fef1ac1727
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-10-15 09:33:53 -0400

    get role by id checks

--- Project0Demo/src/main/java/com/revature/controllers/RoleController.java
+++ Project0Demo/src/main/java/com/revature/controllers/RoleController.java
@@ -12,8 +12,6 @@ public class RoleController {
     //This Handler will handle GET requests to /roles/{id}
     public Handler getRoleByIdHandler = ctx -> {
 
-        //TODO: make sure the id the user sent in is > 0
-
         //extract the path parameter from the HTTP Request URL
         //NOTE: pathParam() returns a String, but we need it as an int
         int role_id = Integer.parseInt(ctx.pathParam("id"));
@@ -21,11 +19,20 @@ public class RoleController {
         //instantiate a Role that holds the data from the specific role ID
         Role role = rDAO.getRoleById(role_id);
 
-        //TODO: make sure the Role that came back isn't null, and maybe some other checks
+        //Make sure the Role that came back isn't null, if so, send a 404 (NOT FOUND)
+        //Make sure the role_id passed in is greater than zero
 
-        //Send the Role back to the client in an HTTP Response
-        ctx.json(role);
-        ctx.status(200); //200 - OK
+        if(role_id <= 0){
+            ctx.result("ID must be greater than zero!");
+            ctx.status(400); //Bad Request
+        } else if (role == null){
+            ctx.result("Role ID: " + role_id + " not found");
+            ctx.status(404);
+        } else {
+            //Send the Role back to the client in an HTTP Response
+            ctx.json(role);
+            ctx.status(200); //200 - OK
+        }
 
     };
 
